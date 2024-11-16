ed273a8723bcdf980353cea8c63d6d5b731ad13f
Suryash Jha <joblesscoder2468@gmail.com>
2024-11-16 17:25:40 +0530

    Webview added, without any media or css for now

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,26 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "auth-vault" is now active!');
+    console.log('Congratulations, your extension "Webview" is up and running now');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('auth-vault.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Suryash!');
-	});
+    let webview = vscode.commands.registerCommand('auth-vault.webview', () => {
 
-	context.subscriptions.push(disposable);
+        let panel = vscode.window.createWebviewPanel("webview", "Web View", {
+            viewColumn: vscode.ViewColumn.One,
+        })
+
+        // will set the html here
+	panel.webview.html = `<h1>This is Heading 1</h1>
+    <h2>This is Suryash 2</h2>
+    <h3>This is Heading 3</h3>
+    <h4>This is Heading 4</h4>
+    <h5>This is Heading 5</h5>`
+
+    });
+
+
+    context.subscriptions.push(webview);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
